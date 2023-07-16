a1313e11aa080c00f76d3b01f96659526c91fedf
Bella Lafrenais <Bella.Lafrenais@reece.com.au>
2023-07-16 21:12:23 +1000

    import csv and parse relevant columns, return book collection on import

--- src/main/kotlin/com/bellalaf/books/BooksApplication.kt
+++ src/main/kotlin/com/bellalaf/books/BooksApplication.kt
@@ -39,11 +39,12 @@ class MessageController(val service: BookService) {
         service.deleteAll()
     }
 
-    @GetMapping("/import")
-    fun import() {
+    @GetMapping("/import-demo")
+    fun import(): List<Book> {
         val input = File ("goodreads.csv").inputStream()
         var books = service.import(input)
         service.saveMultiple(books)
+        return service.findBooks()
     }
 }
 
@@ -66,20 +67,57 @@ class BookService(val db: BookRepository) {
 
     fun import(input: InputStream): List<Book> {
         val reader = input.bufferedReader()
+
+        //read first line - header only
+        reader.readLine()
+
+        //TODO: csv is being annoying - either need to clean the data up, or use a library to handle commas in the data
+        //TODO: also save ISBN and ignore duplicate books
         return reader.lineSequence()
             .filter { it.isNotBlank() }
             .map {
-                val (title, id, author) = it.split(',', ignoreCase = false, limit = 3)
-                Book(text = title.trim().removeSurrounding("\""), id = null)
+                val line = it.split(',', limit = 24)
+                Book(title = cleanUp(line[Goodreads.TITLE.csv_position]),
+                        author = cleanUp(line[Goodreads.AUTHOR.csv_position]),
+                        rating = cleanUp(line[Goodreads.RATING.csv_position]),
+                        num_pages = cleanUp(line[Goodreads.NUM_PAGES.csv_position]),
+                        date_read = cleanUp(line[Goodreads.DATE_READ.csv_position]),
+                        review = cleanUp(line[Goodreads.REVIEW.csv_position]),
+                        shelves = cleanUp(line[Goodreads.BOOKSHELVES.csv_position])
+                )
             }.toList()
     }
+
+
 
     fun saveMultiple(books: List<Book>) {
         books.forEach { save(it) }
     }
+
+    fun cleanUp(str: String): String = str.trim().removeSurrounding("\"")
 }
 
 interface BookRepository : CrudRepository<Book, String>
 
-@Table("BOOKS")
-data class Book(@Id var id: String?, val text: String)
+@Table("BOOKS_2")
+//=null so that we can default construct a value without explictly calling it null
+data class Book(
+        @Id var id: String? = null,
+        val title: String,
+        val author: String? = null,
+        val rating: String? = null,
+        val num_pages: String? = null,
+        val date_read: String? = null,
+        val review: String? = null,
+        val shelves: String? = null,
+        )
+
+enum class Goodreads(val csv_position: Int) {
+    TITLE(1),
+    AUTHOR(2),
+    RATING(7),
+    NUM_PAGES(11),
+    DATE_READ(14),
+    REVIEW(19),
+    BOOKSHELVES(16),
+}
